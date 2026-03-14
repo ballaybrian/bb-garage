fae4894b6fe1f44ab2aaba0a5bb52d8d03d38aac
ballaybrian <ballay.brian@gmail.com>
2026-03-14 16:24:24 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "bb-garage-cache-v1";
+
+const urlsToCache = [
+  "./",
+  "./bb_garage_webapp_standalone_logo_ok.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
